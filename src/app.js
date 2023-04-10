ea08842638306c682368d47f96f822d812ff38ae
Artín Kemanian <artinkemanian@hotmail.com>
2023-04-10 23:56:06 +0000

    Mi primer commit

--- src/app.js
+++ src/app.js
@@ -2,10 +2,35 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+window.onload = () => {
+  document.querySelector("#btn").addEventListener("click", () => {
+    document.querySelector("#excusa").innerHTML = generateExcuse();
+  });
+  console.log("Hello Rigo from the console! ");
+};
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+let generateExcuse = () => {
+  let articulo = ["Mi", "Un/Una", "El/la"];
+  let sustantivo = ["perro", "conductor", "pareja", "ladrón"];
+  let accion = ["robó", "ensució", "mordió", "se llevó"];
+  let posesion = ["mi tarea", "mi auto", "mis zapatos", "mi computadora"];
+  let lugar = ["en mi casa", "en el parque", "en la acera de mi casa"];
+
+  let artIndex = Math.floor(Math.random() * articulo.length);
+  let sustIndex = Math.floor(Math.random() * sustantivo.length);
+  let accIndex = Math.floor(Math.random() * accion.length);
+  let posIndex = Math.floor(Math.random() * posesion.length);
+  let lugIndex = Math.floor(Math.random() * lugar.length);
+
+  return (
+    articulo[artIndex] +
+    " " +
+    sustantivo[sustIndex] +
+    " " +
+    accion[accIndex] +
+    " " +
+    posesion[posIndex] +
+    " " +
+    lugar[lugIndex]
+  );
 };
